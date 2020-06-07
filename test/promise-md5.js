8610b2e247e917c5b55d55fc6908b676b99515dc
Khiet Le <khiet.le@ehochef.com>
2020-06-07 21:05:22 +0100

    use chai-as-promised to write assertions for promise test

--- test/promise-md5.js
+++ test/promise-md5.js
@@ -1,30 +1,32 @@
 var promiseMd5 = require('../promise-md5');
+var chai = require('chai');
 var expect = require('chai').expect;
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 describe('#promiseMd5()', function () {
 
   context('with string argument', function () {
-    it('should compute MD5 hash', async function () {
-      var hash = await promiseMd5('Glad Chinda');
-
-      expect(hash)
-        .to.be.a('string')
+    it('should compute MD5 hash', function () {
+      expect(
+        promiseMd5('Glad Chinda')
+      ).to.eventually.be
+        .a('string')
         .that.matches(/^[a-f0-9]{32}$/)
         .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
     })
-
   })
 
   context('with non-string argument', function () {
-    it('should throw an error', async function () {
+    it('should throw an error', function () {
 
-      await promiseMd5(12345).catch(function (err) {
-        expect(function () {throw err})
-          .to.throw(
-              TypeError,
-              'The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received type number (12345)'
+      expect(
+        promiseMd5(12345)
+      ).to.be.rejectedWith(
+        TypeError,
+        'The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received type number (12345)'
       );
-      });
+
     })
   })
 
