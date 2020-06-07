5742512fb8e58b06485f1d3bc00aea058f04c623
Khiet Le <khiet.le@ehochef.com>
2020-06-07 20:46:18 +0100

    use async/await keyword for promise test

--- test/promise-md5.js
+++ test/promise-md5.js
@@ -4,32 +4,27 @@ var expect = require('chai').expect;
 describe('#promiseMd5()', function () {
 
   context('with string argument', function () {
-    it('should compute MD5 hash', function () {
+    it('should compute MD5 hash', async function () {
+      var hash = await promiseMd5('Glad Chinda');
 
-      return promiseMd5('Glad Chinda')
-        .then(function (hash) {
-          expect(hash)
-            .to.be.a('string')
-            .that.matches(/^[a-f0-9]{32}$/)
-            .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
-        });
-
+      expect(hash)
+        .to.be.a('string')
+        .that.matches(/^[a-f0-9]{32}$/)
+        .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
     })
+
   })
 
   context('with non-string argument', function () {
-    it('should throw an error', function () {
+    it('should throw an error', async function () {
 
-      return promiseMd5(12345)
-        .catch(function (err) {
-          // add an assertion to check the error
-          expect(function () {throw err})
-            .to.throw(
+      await promiseMd5(12345).catch(function (err) {
+        expect(function () {throw err})
+          .to.throw(
               TypeError,
               'The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received type number (12345)'
-            );
-        });
-
+      );
+      });
     })
   })
 
